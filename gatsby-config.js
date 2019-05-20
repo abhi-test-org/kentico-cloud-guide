39c3714b16c4e9dcec47f90ccd7c638bd8c92e18
abhiaiyer91 <abhiaiyer91@gmail.com>
2019-05-20 16:28:54 -0700

    kentico cloud preview

--- gatsby-config.js
+++ gatsby-config.js
@@ -31,6 +31,8 @@ module.exports = {
       resolve: `gatsby-source-kentico-cloud`,
       options: {
         deliveryClientConfig: {
+          enablePreviewMode: true,
+          previewApiKey: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJqdGkiOiJmZTUwYzBkZGMxZDE0MWYzOTVhMTIyNWMyMDJmMzQzNCIsImlhdCI6IjE1NTgzOTI3MzgiLCJleHAiOiIxOTAzOTkyNzM4IiwicHJvamVjdF9pZCI6IjRkYjVhODgyZmM2YjAwZjQ0ZGI3NGE1MmE2MjRmMTVhIiwidmVyIjoiMS4wLjAiLCJhdWQiOiJwcmV2aWV3LmRlbGl2ZXIua2VudGljb2Nsb3VkLmNvbSJ9.sgZJbndh__h1N45RJOIkL3zlWG-Hv1o0KQA6YY4531g',
           projectId:
             process.env.KENTICO_PROJECT_ID ||
             "4db5a882-fc6b-00f4-4db7-4a52a624f15a",
